6f41c4a951003a0caf7271f98707f7a2f8d2b63e
aleksey <aleksey.fgl@gmail.com>
2023-06-29 16:58:51 +0000

    add slider

--- src/common/components/SupperSlider/SupperSlider.tsx
+++ src/common/components/SupperSlider/SupperSlider.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
-
-function valuetext(value: number) {
-    return `${value}°C`;
-}
+import { TextField } from '@mui/material';
+import { styled } from '@mui/styles';
+import { InputNumber } from '../InputNumber/InputNumber';
+import s from './styles.module.scss'
 
 export const RangeSlider = () => {
     const [value, setValue] = useState<number[]>([1, 20]);
+    const [firstValue, setFirstValue] = useState(value[0]);
+    const [secondValue, setSecondValue] = useState(value[1]);
+
+
+    useEffect(() => {
+        setValue([firstValue, secondValue]);
+    }, [firstValue, secondValue]);
 
     const handleChange = (event: Event, newValue: number | number[]) => {
         setValue(newValue as number[]);
@@ -17,9 +24,18 @@ export const RangeSlider = () => {
     return (
         <div>
             <p>Number of cards</p>
-            <Box sx={{ width: 200 }}>
-                <Slider getAriaLabel={() => 'Temperature range'} value={value} onChange={handleChange} valueLabelDisplay="auto" getAriaValueText={valuetext} />
-            </Box>
+            <div className={s.container}>
+                <InputNumber value={value[0]} onChange={setFirstValue} />
+                <Slider value={value}
+                        onChange={handleChange}
+                        valueLabelDisplay='auto'
+                        sx={{width: 200}}
+                />
+                <InputNumber value={value[1]} onChange={setSecondValue} />
+            </div>
         </div>
     );
 };
+
+
+
